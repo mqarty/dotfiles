c14a15c74f25c2513e17fe07eb9769f192522b07
Marty Berryman <mqarty@sewerai.com>
2025-04-17 13:48:12 -0700

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# dotfiles
+
+As advised by Christof Marti at https://github.com/microsoft/vscode-remote-release/issues/8436#issuecomment-1531018498
+VSCode has support for cloning a repo of dotfiles.
+
+And, related docs > https://code.visualstudio.com/docs/devcontainers/containers#_personalizing-with-dotfile-repositories
---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo ".dotfile installation STARTING"
+
+apt-get install -y zsh
+
+cat bashrc.additions >> ~/.bashrc
+
+cp ./.gitconfig ~
+
+# # powerline fonts for zsh agnoster theme
+# git clone https://github.com/powerline/fonts.git
+# cd fonts
+# ./install.sh
+# cd .. && rm -rf fonts
+
+# Install Nerd Fonts
+./fonts.sh
+
+# oh-my-zsh & plugins
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+cp ./.zshrc ~
+
+########################################################################################################################
+#### set agnoster as theme, this came from https://gist.github.com/corentinbettiol/21a6d4e942a0ee58d51acb7996697a88
+#### in vscode settings for devcontainer (not for User or Workspace), Search for terminal.integrated.fontFamily value, and set it to "Roboto Mono for Powerline" (or any of those: https://github.com/powerline/fonts#font-families font families).
+# save current zshrc
+mv ~/.zshrc ~/.zshrc.bak
+
+sudo sh -c "$(wget -O- https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- \
+    -t agnoster
+
+# remove newly created zshrc
+rm -f ~/.zshrc
+# restore saved zshrc
+mv ~/.zshrc.bak ~/.zshrc
+# update theme
+sed -i '/^ZSH_THEME/c\ZSH_THEME="agnoster"' ~/.zshrc
+########################################################################################################################
+
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+
+echo -e "\e[32m"
+echo ".dotfile installation COMPLETE"
+echo -e "\e[0m"
