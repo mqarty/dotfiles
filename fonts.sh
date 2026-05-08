02e04ad199a043c7bba12abed6279135aa7f84d6
Marty Berryman <mqarty@gmail.com>
2026-05-07 23:04:45 -0700

    fix for os; mac vs. devcontainer/ubuntu

--- fonts.sh
+++ fonts.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
-apt install -y fontconfig
+# Skip font installation in devcontainers - fonts must be on the host
+if [ -n "$REMOTE_CONTAINERS" ] || [ -f "/.dockerenv" ]; then
+    echo "Skipping font install in devcontainer - install fonts on host Mac instead"
+    exit 0
+fi
 
 declare -a fonts=(
-#   BitstreamVeraSansMono
-#   CascadiaCode
-#   CodeNewRoman
-#   DroidSansMono
-#   FiraCode
-#   FiraMono
-#   Go-Mono
   Hack
-#   Hermit
-  # JetBrainsMono
-#   Meslo
-#   Noto
-#   Overpass
-#   ProggyClean
   RobotoMono
-#   SourceCodePro
   SpaceMono
-#   Ubuntu
-#   UbuntuMono
 )
 
 version=$(curl -s 'https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest' | jq -r '.name')
@@ -30,22 +18,29 @@ if [ -z "$version" ] || [ "$version" = "null" ]; then
 fi
 echo "latest version: $version"
 
-fonts_dir="${HOME}/.local/share/fonts"
-#fonts_dir="/usr/share/fonts"
-
-if [[ ! -d "$fonts_dir" ]]; then
-  mkdir -p "$fonts_dir"
+# Mac vs Linux font directory
+if [[ "$(uname)" == "Darwin" ]]; then
+    fonts_dir="$HOME/Library/Fonts"
+else
+    fonts_dir="${HOME}/.local/share/fonts"
+    # Install fontconfig on Linux
+    apt install -y fontconfig
 fi
+
+mkdir -p "$fonts_dir"
 
 for font in "${fonts[@]}"; do
   zip_file="${font}.zip"
   download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/${version}/${zip_file}"
   echo "Downloading $download_url"
   wget "$download_url"
-  unzip -o "$zip_file" -d "$fonts_dir"  # Added the -o option here to allow replacing
+  unzip -o "$zip_file" -d "$fonts_dir"
   rm "$zip_file"
 done
 
 find "$fonts_dir" -name 'Windows Compatible' -delete
 
-fc-cache -fv
+# Rebuild font cache on Linux only
+if [[ "$(uname)" != "Darwin" ]]; then
+    fc-cache -fv
+fi
--- install.sh
+++ install.sh
@@ -1,77 +1,66 @@
 #!/bin/bash
 
 echo -e "\e[32m"
-
 echo ".dotfile installation STARTING"
 
-# zsh should already be installed in the Dockerfile, but ensure it's there
+IS_CONTAINER=false
+if [ -n "$REMOTE_CONTAINERS" ] || [ -f "/.dockerenv" ]; then
+    IS_CONTAINER=true
+fi
+
+IS_MAC=false
+if [[ "$(uname)" == "Darwin" ]]; then
+    IS_MAC=true
+fi
+
+# zsh - container has it in Dockerfile, Mac should have it via brew
 if ! command -v zsh &> /dev/null; then
-    echo "Installing zsh..."
-    apt-get update && apt-get install -y zsh
+    if $IS_MAC; then
+        brew install zsh
+    else
+        apt-get update && apt-get install -y zsh
+    fi
 fi
 
 cat bashrc.additions >> ~/.bashrc
-
 cp ./.gitconfig ~
 
-# Install Nerd Fonts
+# Fonts - host Mac only
 ./fonts.sh
 
-# oh-my-zsh & plugins - check if already installed from Dockerfile
-if [ ! -d "/root/.oh-my-zsh" ]; then
-    echo "Installing oh-my-zsh (not found in image)..."
+# oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing oh-my-zsh..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended || true
 else
     echo "oh-my-zsh already installed, updating..."
-    zsh -c 'omz update' || true
+    git -C "$HOME/.oh-my-zsh" pull --rebase || true
 fi
 
-# Copy zshrc (this will have user customizations)
 cp ./.zshrc ~
 
-########################################################################################################################
-#### set agnoster as theme, this came from https://gist.github.com/corentinbettiol/21a6d4e942a0ee58d51acb7996697a88
-#### in vscode settings for devcontainer (not for User or Workspace), Search for terminal.integrated.fontFamily value, and set it to "Roboto Mono for Powerline" (or any of those: https://github.com/powerline/fonts#font-families font families).
-# save current zshrc
-mv ~/.zshrc ~/.zshrc.bak
+# zsh plugins
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 
-sudo sh -c "$(wget -O- https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)" -- \
-    -t agnoster
-
-# remove newly created zshrc
-rm -f ~/.zshrc
-# restore saved zshrc
-mv ~/.zshrc.bak ~/.zshrc
-# update theme
-sed -i '/^ZSH_THEME/c\ZSH_THEME="agnoster"' ~/.zshrc
-########################################################################################################################
-
-# Install zsh plugins if not already installed from Dockerfile
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-    echo "Installing zsh-autosuggestions..."
-    zsh -c 'git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 fi
 
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
-    echo "Installing zsh-syntax-highlighting..."
-    zsh -c 'git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 fi
 
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search" ]; then
-    echo "Installing zsh-history-substring-search..."
-    zsh -c 'git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search'
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-history-substring-search" ]; then
+    git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search "$ZSH_CUSTOM/plugins/zsh-history-substring-search"
 fi
 
-# Install fzf for fuzzy autocomplete if not already installed
-if [ ! -d "~/.fzf" ]; then
-    echo "Installing fzf for fuzzy finding..."
+# fzf
+if [ ! -d "$HOME/.fzf" ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf || true
     ~/.fzf/install --all --no-update-rc 2>/dev/null || true
-else
-    echo "fzf already installed"
 fi
 
-# Enable Terraform autocomplete if terraform is installed
+# Terraform autocomplete
 if command -v terraform >/dev/null 2>&1; then
     echo "Setting up Terraform autocomplete..."
     terraform -install-autocomplete 2>/dev/null || true
